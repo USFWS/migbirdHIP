8646a640fe17e2b773c46ff48fc7bf253659aee9
Abby Walter <lawalter@mymail.vcu.edu>
2021-01-11 14:57:56 -0500

    modified function to allow more than one col name as a uniformity

--- R/validate.R
+++ R/validate.R
@@ -23,20 +23,36 @@ validate <-
       group_by(dl_state, dl_date) %>%
       suppressMessages(summarize(across(contains("bag"), ~n_distinct(.)))) %>%
       mutate(
+        # Create uniformity so the next step has something to paste with
+        uniformity = NA,
+        # Paste col name into "uniformity" if it has only one value per dl_state
+        # and dl_date (the grouping values)
         uniformity =
           case_when(
-            as.numeric(length(unique(ducks_bag))) == 1 ~ "ducks_bag",
-            as.numeric(length(unique(geese_bag))) == 1 ~ "geese_bag",
-            as.numeric(length(unique(dove_bag))) == 1 ~ "dove_bag",
-            as.numeric(length(unique(woodcock_bag))) == 1 ~ "woodcock_bag",
-            as.numeric(length(unique(coots_snipe_bag))) == 1 ~ "coots_snipe_bag",
-            as.numeric(length(unique(rails_gallinules_bag))) == 1 ~ "rails_gallinules_bag",
-            as.numeric(length(unique(cranes_bag))) == 1 ~ "cranes_bag",
-            as.numeric(length(unique(bt_pigeon_bag))) == 1 ~ "bt_pigeon_bag",
-            as.numeric(length(unique(brant_bag))) == 1 ~ "brant_bag",
-            as.numeric(length(unique(seaducks_bag))) == 1 ~ "seaducks_bag",
+            as.numeric(length(unique(ducks_bag))) == 1 ~
+              paste0(uniformity, "-ducks_bag"),
+            as.numeric(length(unique(geese_bag))) == 1 ~
+              paste0(uniformity, "-geese_bag"),
+            as.numeric(length(unique(dove_bag))) == 1 ~
+              paste0(uniformity, "-dove_bag"),
+            as.numeric(length(unique(woodcock_bag))) == 1 ~
+              paste0(uniformity, "-woodcock_bag"),
+            as.numeric(length(unique(coots_snipe_bag))) == 1 ~
+              paste0(uniformity, "-coots_snipe_bag"),
+            as.numeric(length(unique(rails_gallinules_bag))) == 1 ~
+              paste0(uniformity, "-rails_gallinules_bag"),
+            as.numeric(length(unique(cranes_bag))) == 1 ~
+              paste0(uniformity, "-cranes_bag"),
+            as.numeric(length(unique(bt_pigeon_bag))) == 1 ~
+              paste0(uniformity, "-bt_pigeon_bag"),
+            as.numeric(length(unique(brant_bag))) == 1 ~
+              paste0(uniformity, "-brant_bag"),
+            as.numeric(length(unique(seaducks_bag))) == 1 ~
+              paste0(uniformity, "-seaducks_bag"),
             TRUE ~ NA_character_
-          )
+          ),
+        # Remove unnecessary leading NA- strings from the first step
+        uniformity = str_remove(uniformity, "NA\\-")
       ) %>%
       filter(!is.na(uniformity)) %>%
       select(dl_state, dl_date, uniformity) %>%
