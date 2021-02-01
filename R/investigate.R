8acfc4d5c6898064b4674119abda748d372cc81d
Abby Walter <lawalter@mymail.vcu.edu>
2021-02-01 15:41:37 -0500

    updated to reflect changes in validate function. will now work with either dl_date or dl_cycle as input

--- R/investigate.R
+++ R/investigate.R
@@ -9,7 +9,11 @@
 #'
 #' @param x The object created after tidying data with \code{\link{tidy}}
 #' @param loc The download state in question
-#' @param date The download date in question
+#' @param period_type The type of time period that will be given. You may choose one of two types:
+#' \itemize{
+#' \item dl_date
+#' \item dl_cycle}
+#' @param period Value of the time period in question
 #' @param species The bird group in question. One of the bird groups from the following list may be supplied:
 #' \itemize{
 #' \item ducks_bag, geese_bag, dove_bag, woodcock_bag, coots_snipe, rails_gallinules, cranes, band_tailed_pigeon, brant, seaducks}
@@ -20,12 +24,12 @@
 #' @export
 
 investigate <-
-  function(x, loc, date, species){
+  function(x, loc, period_type, period, species){
 
     investigated_x <-
       x %>%
-      select(dl_state, dl_date, ducks_bag:seaducks) %>%
-      filter(dl_state == loc & dl_date == date) %>%
+      select(dl_state, {{period_type}}, ducks_bag:seaducks) %>%
+      filter(dl_state == loc & !!sym(period_type) == period) %>%
       select(quo_name(species)) %>%
       distinct()
 
