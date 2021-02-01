c583ecc1e342920a73be4d3ae3ec1066e6fc657d
Abby Walter <lawalter@mymail.vcu.edu>
2021-02-01 15:35:52 -0500

    added ability to specify timeframe and removed messages from output

--- R/validate.R
+++ R/validate.R
@@ -13,6 +13,11 @@
 #'  \item vertical - Checks for repetition vertically in species and/or bag fields, grouped by dl_state and dl_date
 #'  \item horizontal - Checks for repetition horizontally, across each record
 #'  }
+#' @param period Time period in which to group the data. The function uses dl_date automatically, but either of the following may be supplied:
+#'  \itemize{
+#'  \item dl_date - Date the HIP data were downloaded
+#'  \item dl_cycle - Download cycle that the HIP data belong to
+#'  }
 #'
 #' @author Abby Walter, \email{abby_walter@@fws.gov}
 #' @references \url{https://github.com/USFWS/migbirdHarvestData}
@@ -20,53 +25,81 @@
 #' @export
 
 validate <-
-  function(x, type){
+  function(x, type, period = NA){
     if(type == "vertical"){
 
       # Vertical validation
 
-      validated_v <-
-        x %>%
-        # Subset the data
-        select(
-          dl_state,
-          dl_date,
-          matches("bag|coots|rails|cranes|pigeon|brant|seaducks")) %>%
-        group_by(dl_state, dl_date) %>%
-        # Add number of rows per group
-        mutate(dl_rows = n()) %>%
-        # Move dl_rows to 3rd col position
-        relocate(dl_rows, .after = dl_date) %>%
-        ungroup() %>%
-        group_by(dl_state, dl_date, dl_rows) %>%
-        # Count the number of unique values in each species column
-        summarize(across(ducks_bag:seaducks, ~length(unique(.x))), .groups = "keep") %>%
-        pivot_longer(
-          cols = !contains("dl"),
-          names_to = "species_grp",
-          values_to = "v_uniformity") %>%
-        ungroup() %>%
-        # Keep only the uniform spp groups
-        filter(v_uniformity == 1) %>%
-        # Report count of times uniform values are repeated
-        mutate(v_uniform = dl_rows) %>%
-        select(-c("dl_rows", "v_uniformity"))
-
-      if(nrow(validated_v) != 0) {
-
-        message(
-          paste0(
-            "Attention: Uniform value detected within one or more fields, ",
-            "please review.")
-        )
-
-        return(validated_v)
-
+      # Without time period specification
+      if(is.na(period)){
+        validated_v <-
+          x %>%
+          # Subset the data
+          select(
+            dl_state,
+            dl_date,
+            matches("bag|coots|rails|cranes|pigeon|brant|seaducks")) %>%
+          group_by(dl_state, dl_date) %>%
+          # Add number of rows per group
+          mutate(dl_rows = n()) %>%
+          # Move dl_rows to 3rd col position
+          relocate(dl_rows, .after = dl_date) %>%
+          ungroup() %>%
+          group_by(dl_state, dl_date, dl_rows) %>%
+          # Count the number of unique values in each species column
+          summarize(across(ducks_bag:seaducks, ~length(unique(.x))), .groups = "keep") %>%
+          pivot_longer(
+            cols = !contains("dl"),
+            names_to = "species_grp",
+            values_to = "v_uniformity") %>%
+          ungroup() %>%
+          # Keep only the uniform spp groups
+          filter(v_uniformity == 1) %>%
+          # Report count of times uniform values are repeated
+          mutate(v_uniform = dl_rows) %>%
+          select(-c("dl_rows", "v_uniformity"))
       }
+
+      # With time period specification: dl_date or dl_cycle
+      else if(period == "dl_date" | period == "dl_cycle"){
+        validated_v <-
+          x %>%
+          # Subset the data
+          select(
+            dl_state,
+            {{period}},
+            matches("bag|coots|rails|cranes|pigeon|brant|seaducks")) %>%
+          group_by(dl_state, !!sym(period)) %>%
+          # Add number of rows per group
+          mutate(dl_rows = n()) %>%
+          # Move dl_rows to 3rd col position
+          relocate(dl_rows, .after = {{period}}) %>%
+          ungroup() %>%
+          group_by(dl_state, !!sym(period), dl_rows) %>%
+          # Count the number of unique values in each species column
+          summarize(across(ducks_bag:seaducks, ~length(unique(.x))), .groups = "keep") %>%
+          pivot_longer(
+            cols = !contains("dl"),
+            names_to = "species_grp",
+            values_to = "v_uniformity") %>%
+          ungroup() %>%
+          # Keep only the uniform spp groups
+          filter(v_uniformity == 1) %>%
+          # Report count of times uniform values are repeated
+          mutate(v_uniform = dl_rows) %>%
+          select(-c("dl_rows", "v_uniformity"))
+      }
+
+      # Return for uniformity detected
+      if(nrow(validated_v) != 0) {
+       return(validated_v)
+      }
+      # Return for no uniformities
       else{
         message("Data are good to go! No vertical repetition detected.")
       }
     }
+
     else if(type == "horizontal"){
 
       # Horizontal validation
@@ -99,22 +132,17 @@ validate <-
         summarize(h_uniform = n(), .groups = "keep") %>%
         ungroup()
 
+      # Return for uniformity detected
       if(nrow(validated_h) != 0) {
-
-        message(
-          paste0(
-            "Attention: Uniform value detected across one or more records, ",
-            "please review.")
-        )
-
         return(validated_h)
-
       }
+      # Return for no uniformities
       else{
         message("Data are good to go! No horizontal repetition detected.")
       }
-
     }
+
+    # Return for incorrect type given
     else{
       message("Incorrect type of validation supplied.")
     }
