86890322611ab59b6b0fd36dc97bb71e3ca8fbca
Abby Walter <lawalter@mymail.vcu.edu>
2021-02-11 16:24:32 -0500

    removed the ability to plot by count for outOfStateHunters; instead, the plot output is by proportion only. bar labels are counts

--- R/outOfStateHunters.R
+++ R/outOfStateHunters.R
@@ -1,6 +1,6 @@
 #' Find out-of-state hunters
 #'
-#' Create a tibble and plot of hunters who have reported hunting migratory waterfowl outside of the state associated with their address.
+#' Create a tibble and plot of hunters who have registered outside of their home state.
 #'
 #' @import dplyr
 #' @import tidyr
@@ -10,12 +10,6 @@
 #' @importFrom stats reorder
 #'
 #' @param x A proofed data table created by \code{\link{proof}}
-#' @param type Type of plot to create
-#' Acceptable values include:
-#'  \itemize{
-#'  \item proportion - Number of out-of-state hunters divided by the total hunters per state
-#'  \item count - Count of out-of-state hunters per state
-#'  }
 #'
 #' @author Abby Walter, \email{abby_walter@@fws.gov}
 #' @references \url{https://github.com/USFWS/migbirdHarvestData}
@@ -23,108 +17,74 @@
 #' @export
 
 outOfStateHunters <-
-  function(x, type){
+  function(x){
 
     # Tibble of hunters from out-of-state
+
     out_of_staters <-
       suppressWarnings(
+        # Suppress summarize groups message
         suppressMessages(
           x %>%
             select(state, dl_state) %>%
+            # Filter to out-of-staters
             filter(state != dl_state) %>%
+            # Count
             group_by(dl_state) %>%
             summarize(out_of_state = n()) %>%
             ungroup()
         )
       )
 
-    if(type == "count"){
+    # Proportion calculation
 
-      # Out of state hunter count plot
-
-      oos_plot <-
-        out_of_staters %>%
-        ggplot(aes(x = reorder(dl_state, out_of_state))) +
-        geom_bar(
-          aes(
-            y = out_of_state,
-            x = reorder(dl_state, out_of_state)),
-          stat = "identity") +
-        geom_text(
-          aes(
-            y = out_of_state,
-            x = reorder(dl_state, out_of_state),
-            label = out_of_state,
-            angle = 90),
-          vjust = 0.2,
-          hjust = -0.2) +
-        labs(
-          x = "State",
-          y = "Out-of-state hunters (count)",
-          title = "Hunters with addresses not in the download state") +
-        scale_y_continuous(expand = expansion(mult = c(-0, 0.2))) +
-        theme_classic() +
-        theme(
-          axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1))
-
-      print(oos_plot)
-      return(out_of_staters)
-    }
-
-    else if(type == "proportion"){
-
-      # Proportion calculation
-
-      oos_proportion <-
-        suppressWarnings(
-          suppressMessages(
-            x %>%
-              select(dl_state) %>%
-              group_by(dl_state) %>%
-              summarize(n_total = n()) %>%
-              ungroup() %>%
-              left_join(out_of_staters, by = "dl_state") %>%
-              mutate(
-                outofstate_prop = out_of_state/n_total,
-                outofstate_prop = round(outofstate_prop, digits = 2)
-              ) %>%
-              select(-out_of_state) %>%
-              filter(!is.na(outofstate_prop))
-            )
+    oos_proportion <-
+      suppressWarnings(
+        # Suppress summarize .groups message
+        suppressMessages(
+          x %>%
+            select(dl_state) %>%
+            group_by(dl_state) %>%
+            summarize(n_total = n()) %>%
+            ungroup() %>%
+            left_join(out_of_staters, by = "dl_state") %>%
+            mutate(
+              outofstate_prop = out_of_state/n_total,
+              outofstate_prop = round(outofstate_prop, digits = 2)
+            ) %>%
+            select(-out_of_state) %>%
+            filter(!is.na(outofstate_prop))
           )
+        )
 
-      # Out of state hunter proportion plot
+    # Out of state hunter proportion plot
 
-      oos_plot <-
-        oos_proportion %>%
-        ggplot(aes(x = reorder(dl_state, outofstate_prop))) +
-        geom_bar(
-          aes(
-            y = outofstate_prop,
-            x = reorder(dl_state, outofstate_prop)),
-          stat = "identity") +
-        geom_text(
-          aes(
-            y = outofstate_prop,
-            x = reorder(dl_state, outofstate_prop),
-            label = outofstate_prop,
-            angle = 90),
-          vjust = 0.2,
-          hjust = -0.2) +
-        labs(
-          x = "State",
-          y = "Out-of-state hunters (proportion)",
-          title = "Hunters with addresses not in the download state") +
-        scale_y_continuous(expand = expansion(mult = c(-0, 0.2))) +
-        theme_classic() +
-        theme(
-          axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1))
-
+    oos_plot <-
+      oos_proportion %>%
+      ggplot(aes(x = reorder(dl_state, outofstate_prop))) +
+      geom_bar(
+        aes(
+          y = outofstate_prop,
+          x = reorder(dl_state, outofstate_prop)),
+        stat = "identity") +
+      geom_text(
+        aes(
+          y = outofstate_prop,
+          x = reorder(dl_state, outofstate_prop),
+          label = n_total,
+          angle = 90),
+        vjust = 0.2,
+        hjust = -0.2) +
+      labs(
+        x = "State",
+        y = "Out-of-state hunters (proportion)",
+        title = "Hunters with addresses not in the download state") +
+      scale_y_continuous(expand = expansion(mult = c(-0, 0.2))) +
+      theme_classic() +
+      theme(
+        axis.text.x = element_text(angle = 45,vjust = 1,hjust = 1))
       print(oos_plot)
-      return(oos_proportion)
-    }
 
-    else{
-      message("Error: Invalid plot type supplied.")
-    }
+    return(oos_proportion)
+
   }
