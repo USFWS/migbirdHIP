dab6d33fb2811abf01aa0c7c3db0aefdd06110ca
Abby Walter <lawalter@mymail.vcu.edu>
2021-06-22 15:42:43 -0400

    correct function: added parameter for output to contain either state bag values or fws strata

--- R/correct.R
+++ R/correct.R
@@ -19,6 +19,7 @@
 #'
 #' @param x The object created after error flagging data with \code{\link{proof}}
 #' @param year The year in which the Harvest Information Program data were collected
+#' @param data Do you want the frame to include original state `bag` values or FWS `strata`?
 #'
 #' @author Abby Walter, \email{abby_walter@@fws.gov}
 #' @references \url{https://github.com/USFWS/migbirdHarvestData}
@@ -26,8 +27,130 @@
 #' @export
 
 correct <-
-  function(x, year){
+  function(x, year, data){
 
+    if(data == "bag"){
+      corrected_x <-
+        x %>%
+        mutate(
+          # Change NAs in errors col to "none" so that str_detect functions work
+          errors =
+            ifelse(is.na(errors), "none", errors),
+          # Title correction
+          title =
+            # Set to NA if title is flagged
+            ifelse(str_detect(errors, "title"), NA, title),
+          # Suffix correction
+          suffix =
+            # Set to NA if suffix is flagged
+            ifelse(str_detect(errors, "suffix"), NA, suffix),
+          # Change "none"s back to NAs in errors col
+          errors =
+            ifelse(errors == "none", NA, errors),
+          # Zip code correction
+          zip =
+            # Insert a hyphen in continuous 9 digit zip codes
+            ifelse(
+              str_detect(zip, "^[0-9]{9}$"),
+              paste0(
+                str_extract(zip, "^[0-9]{5}"),
+                "-",
+                str_extract(zip,"[0-9]{4}$")),
+              zip),
+          zip =
+            # Insert a hyphen in 9 digit zip codes with a middle space
+            ifelse(
+              str_detect(zip, "^[0-9]{5}\\s[0-9]{4}$"),
+              str_replace(zip, "\\s", "\\-"),
+              zip),
+          zip =
+            # Remove trailing -0000
+            ifelse(
+              str_detect(zip, "\\-0000"),
+              str_remove(zip, "\\-0000"),
+              zip),
+          zip =
+            # Remove trailing -___
+            ifelse(
+              str_detect(zip, "\\-\\_+"),
+              str_remove(zip, "\\-\\_+"),
+              zip),
+          # Email
+          email =
+            # Delete spaces
+            str_remove_all(email, " "),
+          email =
+            # Delete slashes /
+            str_remove_all(email, "\\/"),
+          email =
+            # Delete commas
+            str_remove_all(email, "\\,"),
+          email =
+            # To lowercase
+            tolower(email),
+          email =
+            # Fix multiple @
+            ifelse(
+              str_detect(email, "\\@\\@+"),
+              str_replace(email, "\\@\\@+", "\\@"),
+              email),
+          email =
+            # Add endings to common domains
+            case_when(
+              str_detect(email, "\\@gmail$") ~
+                str_replace(email, "\\@gmail$", "\\@gmail\\.com"),
+              str_detect(email, "\\@yahoo$") ~
+                str_replace(email, "\\@yahoo$", "\\@yahoo\\.com"),
+              str_detect(email, "\\@aol$") ~
+                str_replace(email, "\\@aol$", "\\@aol\\.com"),
+              str_detect(email, "\\@comcast$") ~
+                str_replace(email, "\\@comcast$", "\\@comcast\\.net"),
+              str_detect(email, "\\@verizon$") ~
+                str_replace(email, "\\@verizon$", "\\@verizon\\.net"),
+              str_detect(email, "\\@cox$") ~
+                str_replace(email, "\\@cox$", "\\@cox\\.net"),
+              str_detect(email, "\\@outlook$") ~
+                str_replace(email, "\\@outlook$", "\\@outlook\\.com"),
+              str_detect(email, "\\@hotmail$") ~
+                str_replace(email, "\\@hotmail$", "\\@hotmail\\.com"),
+              TRUE ~ email
+            ),
+          email =
+            # Fix punctuation
+            case_when(
+              # Correct .ccom typos
+              str_detect(email, "(?<=\\.)ccom$") ~
+                str_replace(email, "ccom$", "com"),
+              # Add period if missing
+              str_detect(email, "(?<=[^\\.])com$") ~
+                str_replace(email, "com$", "\\.com"),
+              str_detect(email, "(?<=[^\\.])net$") ~
+                str_replace(email, "net$", "\\.net"),
+              str_detect(email, "(?<=[^\\.])edu$") ~
+                str_replace(email, "edu$", "\\.edu"),
+              str_detect(email, "(?<=[^\\.])gov$") ~
+                str_replace(email, "gov$", "\\.gov"),
+              str_detect(email, "(?<=[^\\.])org$") ~
+                str_replace(email, "org$", "\\.org"),
+              TRUE ~ email
+            ),
+          email =
+            # Replace with NA
+            case_when(
+              # If there is no @
+              !str_detect(email, "\\@") ~ NA_character_,
+              # If the email is invalid
+              str_detect(email, "noemail\\@") ~ NA_character_,
+              str_detect(email, "^none@none$") ~ NA_character_,
+              # If there are any remaining "NA" type strings
+              str_detect(email, "n\\/a") ~ NA_character_,
+              # If there is only an @
+              str_detect(email, "^@$") ~ NA_character_,
+              TRUE ~ email
+            )
+        )
+    }
+    else if(data == "strata"){
     corrected_x <-
       x %>%
       mutate(
@@ -250,6 +373,10 @@ correct <-
       ) %>%
       mutate(seaducks = FWSstratum) %>%
       select(-FWSstratum)
+    }
+    else{
+      message("Invalid data type provided, please input 'bag' or 'strata'.")
+    }
 
     # Re-run the proof script to get an updated errors column
 
