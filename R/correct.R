c92b0f6fdea71b1eebb3779d41edb541b1aaf9ef
Abby Walter <lawalter@mymail.vcu.edu>
2021-08-24 09:32:41 -0400

    correct function: recycled a portion of script and added error reporting for bad zip codes

--- R/correct.R
+++ R/correct.R
@@ -2,6 +2,7 @@
 #'
 #' After flagging errors in the data with \code{\link{proof}}, attempt corrections in all fields. Errors that cannot be programmatically corrected will be reported for manual correction.
 #'
+#' @importFrom magrittr %<>%
 #' @importFrom dplyr %>%
 #' @importFrom dplyr mutate
 #' @importFrom dplyr case_when
@@ -14,7 +15,6 @@
 #' @importFrom stringr str_detect
 #' @importFrom stringr str_extract
 #' @importFrom stringr str_replace
-#' @importFrom stringr str_remove
 #' @importFrom stringr str_remove_all
 #'
 #' @param x The object created after error flagging data with \code{\link{proof}}
@@ -29,101 +29,101 @@
 correct <-
   function(x, year, data){
 
+    corrected_x <-
+      x %>%
+      mutate(
+        # Change NAs in errors col to "none" so that str_detect functions work
+        errors =
+          ifelse(is.na(errors), "none", errors),
+        # Title correction
+        title =
+          # Set to NA if title is flagged
+          ifelse(str_detect(errors, "title"), NA, title),
+        # Suffix correction
+        suffix =
+          # Set to NA if suffix is flagged
+          ifelse(str_detect(errors, "suffix"), NA, suffix),
+        # Change "none"s back to NAs in errors col
+        errors =
+          ifelse(errors == "none", NA, errors),
+        # Email
+        email =
+          # Delete spaces
+          str_remove_all(email, " "),
+        email =
+          # Delete slashes /
+          str_remove_all(email, "\\/"),
+        email =
+          # Delete commas
+          str_remove_all(email, "\\,"),
+        email =
+          # To lowercase
+          tolower(email),
+        email =
+          # Fix multiple @
+          ifelse(
+            str_detect(email, "\\@\\@+"),
+            str_replace(email, "\\@\\@+", "\\@"),
+            email),
+        email =
+          # Add endings to common domains
+          case_when(
+            str_detect(email, "\\@gmail$") ~
+              str_replace(email, "\\@gmail$", "\\@gmail\\.com"),
+            str_detect(email, "\\@yahoo$") ~
+              str_replace(email, "\\@yahoo$", "\\@yahoo\\.com"),
+            str_detect(email, "\\@aol$") ~
+              str_replace(email, "\\@aol$", "\\@aol\\.com"),
+            str_detect(email, "\\@comcast$") ~
+              str_replace(email, "\\@comcast$", "\\@comcast\\.net"),
+            str_detect(email, "\\@verizon$") ~
+              str_replace(email, "\\@verizon$", "\\@verizon\\.net"),
+            str_detect(email, "\\@cox$") ~
+              str_replace(email, "\\@cox$", "\\@cox\\.net"),
+            str_detect(email, "\\@outlook$") ~
+              str_replace(email, "\\@outlook$", "\\@outlook\\.com"),
+            str_detect(email, "\\@hotmail$") ~
+              str_replace(email, "\\@hotmail$", "\\@hotmail\\.com"),
+            TRUE ~ email
+          ),
+        email =
+          # Fix punctuation
+          case_when(
+            # Correct .ccom typos
+            str_detect(email, "(?<=\\.)ccom$") ~
+              str_replace(email, "ccom$", "com"),
+            # Add period if missing
+            str_detect(email, "(?<=[^\\.])com$") ~
+              str_replace(email, "com$", "\\.com"),
+            str_detect(email, "(?<=[^\\.])net$") ~
+              str_replace(email, "net$", "\\.net"),
+            str_detect(email, "(?<=[^\\.])edu$") ~
+              str_replace(email, "edu$", "\\.edu"),
+            str_detect(email, "(?<=[^\\.])gov$") ~
+              str_replace(email, "gov$", "\\.gov"),
+            str_detect(email, "(?<=[^\\.])org$") ~
+              str_replace(email, "org$", "\\.org"),
+            TRUE ~ email
+          ),
+        email =
+          # Replace with NA
+          case_when(
+            # If there is no @
+            !str_detect(email, "\\@") ~ NA_character_,
+            # If the email is invalid
+            str_detect(email, "noemail\\@") ~ NA_character_,
+            str_detect(email, "^none@none$") ~ NA_character_,
+            # If there are any remaining "NA" type strings
+            str_detect(email, "n\\/a") ~ NA_character_,
+            # If there is only an @
+            str_detect(email, "^@$") ~ NA_character_,
+            TRUE ~ email
+          )
+      )
+
     if(data == "bag"){
-      corrected_x <-
-        x %>%
-        mutate(
-          # Change NAs in errors col to "none" so that str_detect functions work
-          errors =
-            ifelse(is.na(errors), "none", errors),
-          # Title correction
-          title =
-            # Set to NA if title is flagged
-            ifelse(str_detect(errors, "title"), NA, title),
-          # Suffix correction
-          suffix =
-            # Set to NA if suffix is flagged
-            ifelse(str_detect(errors, "suffix"), NA, suffix),
-          # Change "none"s back to NAs in errors col
-          errors =
-            ifelse(errors == "none", NA, errors),
-          # Email
-          email =
-            # Delete spaces
-            str_remove_all(email, " "),
-          email =
-            # Delete slashes /
-            str_remove_all(email, "\\/"),
-          email =
-            # Delete commas
-            str_remove_all(email, "\\,"),
-          email =
-            # To lowercase
-            tolower(email),
-          email =
-            # Fix multiple @
-            ifelse(
-              str_detect(email, "\\@\\@+"),
-              str_replace(email, "\\@\\@+", "\\@"),
-              email),
-          email =
-            # Add endings to common domains
-            case_when(
-              str_detect(email, "\\@gmail$") ~
-                str_replace(email, "\\@gmail$", "\\@gmail\\.com"),
-              str_detect(email, "\\@yahoo$") ~
-                str_replace(email, "\\@yahoo$", "\\@yahoo\\.com"),
-              str_detect(email, "\\@aol$") ~
-                str_replace(email, "\\@aol$", "\\@aol\\.com"),
-              str_detect(email, "\\@comcast$") ~
-                str_replace(email, "\\@comcast$", "\\@comcast\\.net"),
-              str_detect(email, "\\@verizon$") ~
-                str_replace(email, "\\@verizon$", "\\@verizon\\.net"),
-              str_detect(email, "\\@cox$") ~
-                str_replace(email, "\\@cox$", "\\@cox\\.net"),
-              str_detect(email, "\\@outlook$") ~
-                str_replace(email, "\\@outlook$", "\\@outlook\\.com"),
-              str_detect(email, "\\@hotmail$") ~
-                str_replace(email, "\\@hotmail$", "\\@hotmail\\.com"),
-              TRUE ~ email
-            ),
-          email =
-            # Fix punctuation
-            case_when(
-              # Correct .ccom typos
-              str_detect(email, "(?<=\\.)ccom$") ~
-                str_replace(email, "ccom$", "com"),
-              # Add period if missing
-              str_detect(email, "(?<=[^\\.])com$") ~
-                str_replace(email, "com$", "\\.com"),
-              str_detect(email, "(?<=[^\\.])net$") ~
-                str_replace(email, "net$", "\\.net"),
-              str_detect(email, "(?<=[^\\.])edu$") ~
-                str_replace(email, "edu$", "\\.edu"),
-              str_detect(email, "(?<=[^\\.])gov$") ~
-                str_replace(email, "gov$", "\\.gov"),
-              str_detect(email, "(?<=[^\\.])org$") ~
-                str_replace(email, "org$", "\\.org"),
-              TRUE ~ email
-            ),
-          email =
-            # Replace with NA
-            case_when(
-              # If there is no @
-              !str_detect(email, "\\@") ~ NA_character_,
-              # If the email is invalid
-              str_detect(email, "noemail\\@") ~ NA_character_,
-              str_detect(email, "^none@none$") ~ NA_character_,
-              # If there are any remaining "NA" type strings
-              str_detect(email, "n\\/a") ~ NA_character_,
-              # If there is only an @
-              str_detect(email, "^@$") ~ NA_character_,
-              TRUE ~ email
-            )
-        )
 
       # Re-run the proof script to get an updated errors column
-
       corrproof_bag_x <-
         proof(corrected_x, year = year) %>%
         # Proof the zip codes -- are they associated with the correct states?
@@ -142,106 +142,29 @@ correct <-
                 paste0(errors, "-zip"),
               TRUE ~ errors)
         ) %>%
+
+      # Error check: are any zip codes wrong?
+      if(TRUE %in% (corrproof_bag_x$state != corrproof_bag_x$zipState)){
+        message(
+          paste0("Warning: Zip codes detected that do not correspond to",
+                 "provided state of residence."))
+        print(
+          corrproof_bag_x %>%
+            select(record_key, zip, state, zipState) %>%
+            filter(state != zipState))
+      }
+
+      corrproof_bag_x %<>%
         select(-c("zipPrefix", "zipState"))
 
       return(corrproof_bag_x)
 
     }
     else if(data == "strata"){
-      corrected_x <-
-        x %>%
-        mutate(
-          # Change NAs in errors col to "none" so that str_detect functions work
-          errors =
-            ifelse(is.na(errors), "none", errors),
-          # Title correction
-          title =
-            # Set to NA if title is flagged
-            ifelse(str_detect(errors, "title"), NA, title),
-          # Suffix correction
-          suffix =
-            # Set to NA if suffix is flagged
-            ifelse(str_detect(errors, "suffix"), NA, suffix),
-          # Change "none"s back to NAs in errors col
-          errors =
-            ifelse(errors == "none", NA, errors),
-          # Email
-          email =
-            # Delete spaces
-            str_remove_all(email, " "),
-          email =
-            # Delete slashes /
-            str_remove_all(email, "\\/"),
-          email =
-            # Delete commas
-            str_remove_all(email, "\\,"),
-          email =
-            # To lowercase
-            tolower(email),
-          email =
-            # Fix multiple @
-            ifelse(
-              str_detect(email, "\\@\\@+"),
-              str_replace(email, "\\@\\@+", "\\@"),
-              email),
-          email =
-            # Add endings to common domains
-            case_when(
-              str_detect(email, "\\@gmail$") ~
-                str_replace(email, "\\@gmail$", "\\@gmail\\.com"),
-              str_detect(email, "\\@yahoo$") ~
-                str_replace(email, "\\@yahoo$", "\\@yahoo\\.com"),
-              str_detect(email, "\\@aol$") ~
-                str_replace(email, "\\@aol$", "\\@aol\\.com"),
-              str_detect(email, "\\@comcast$") ~
-                str_replace(email, "\\@comcast$", "\\@comcast\\.net"),
-              str_detect(email, "\\@verizon$") ~
-                str_replace(email, "\\@verizon$", "\\@verizon\\.net"),
-              str_detect(email, "\\@cox$") ~
-                str_replace(email, "\\@cox$", "\\@cox\\.net"),
-              str_detect(email, "\\@outlook$") ~
-                str_replace(email, "\\@outlook$", "\\@outlook\\.com"),
-              str_detect(email, "\\@hotmail$") ~
-                str_replace(email, "\\@hotmail$", "\\@hotmail\\.com"),
-              TRUE ~ email
-            ),
-          email =
-            # Fix punctuation
-            case_when(
-              # Correct .ccom typos
-              str_detect(email, "(?<=\\.)ccom$") ~
-                str_replace(email, "ccom$", "com"),
-              # Add period if missing
-              str_detect(email, "(?<=[^\\.])com$") ~
-                str_replace(email, "com$", "\\.com"),
-              str_detect(email, "(?<=[^\\.])net$") ~
-                str_replace(email, "net$", "\\.net"),
-              str_detect(email, "(?<=[^\\.])edu$") ~
-                str_replace(email, "edu$", "\\.edu"),
-              str_detect(email, "(?<=[^\\.])gov$") ~
-                str_replace(email, "gov$", "\\.gov"),
-              str_detect(email, "(?<=[^\\.])org$") ~
-                str_replace(email, "org$", "\\.org"),
-              TRUE ~ email
-            ),
-          email =
-            # Replace with NA
-            case_when(
-              # If there is no @
-              !str_detect(email, "\\@") ~ NA_character_,
-              # If there are two @ (two emails submitted)
-              str_detect(email, "\\@.+\\@") ~ NA_character_,
-              # If the email is invalid
-              str_detect(email, "noemail\\@") ~ NA_character_,
-              str_detect(email, "^none@none$") ~ NA_character_,
-              # If there are any remaining "NA" type strings
-              str_detect(email, "n\\/a") ~ NA_character_,
-              # If there is only an @
-              str_detect(email, "^@$") ~ NA_character_,
-              TRUE ~ email
-            )
-        ) %>%
-        # Species group bag corrections...
+
+      corrected_strata_x <-
+        corrected_x %>%
+        # Species group bag corrections:
         # Use internal data from hip_bags_ref to join correct FWS strata,
         # replacing the variable state strata
         # Bag correction: ducks
@@ -370,6 +293,20 @@ correct <-
         rename_at(
           vars(contains("_bag")),
           ~str_replace(., "_bag", "_strata"))
+
+      # Error check: are any zip codes wrong?
+      if(TRUE %in% (corrproof_strata_x$state != corrproof_strata_x$zipState)){
+        message(
+          paste0("Warning: Zip codes detected that do not correspond to",
+                 "provided state of residence."))
+        print(
+          corrproof_strata_x %>%
+            select(record_key, zip, state, zipState) %>%
+            filter(state != zipState))
+      }
+
+      corrproof_strata_x %<>%
+        select(-c("zipPrefix", "zipState"))
 
       return(corrproof_strata_x)
     }
