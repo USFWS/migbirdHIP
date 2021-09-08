0f9229764ed09390b434e846d5f2b45a8700b10d
Abby Walter <lawalter@mymail.vcu.edu>
2021-09-08 16:17:33 -0400

    pullErrors function: allowed output to not be distincted so that the dl_report template works

--- R/pullErrors.R
+++ R/pullErrors.R
@@ -2,6 +2,7 @@
 #'
 #' Pull and view errors that have been flagged for a particular field. This function allows you to easily see what the \code{\link{proof}} function has determined to be unacceptable data.
 #'
+#' @importFrom magrittr %<>%
 #' @importFrom dplyr %>%
 #' @importFrom dplyr filter
 #' @importFrom dplyr select
@@ -14,6 +15,7 @@
 #' @param error Field that should be pulled. One of the fields from the following list may be supplied:
 #' \itemize{
 #' \item title, firstname, middle, lastname, suffix, address, city, state, zip, birth_date, issue_date, hunt_mig_birds, ducks_bag, geese_bag, dove_bag, woodcock_bag, coots_snipe_bag, rails_gallinules_bag, cranes, band_tailed_pigeon, brant, seaducks, registration_year, email}
+#' @param distinct If FALSE, returns all error vvalues ; if TRUE (default), only returns unique values.
 #'
 #' @author Abby Walter, \email{abby_walter@@fws.gov}
 #' @references \url{https://github.com/USFWS/migbirdHarvestData}
@@ -21,16 +23,23 @@
 #' @export
 
 pullErrors <-
-  function(x, error){
+  function(x, error, distinct = TRUE){
 
     pulled_error <-
       x %>%
       select(error, errors) %>%
       filter(str_detect(errors, error)) %>%
-      select(error) %>%
-      distinct() %>%
-      pull()
+      select(error)
 
+    if(distinct == TRUE){
+      pulled_error %<>%
+        distinct() %>%
+        pull()
+      }
+    else{
+      pulled_error %<>%
+        pull()
+      }
     if(is_empty(pulled_error)){
       message("Success! All values are correct.")
       }
