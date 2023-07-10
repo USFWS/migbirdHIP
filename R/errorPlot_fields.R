5f0cc10ea60f501d64550f3970a7e5040a95d1bd
lawalter <abby_walter@fws.gov>
2023-07-10 16:33:33 -0400

    errorPlot_fields function: replaced %>% with |>; switched to tidyr::separate_wider_delim to avoid the superseded tidyr::separate, which caused a lot of warnings; added a user-friendly warning message if supplying a bad value to loc param; used .groups arg in summarize to quiet dplyr messages; reduced redundancy overall

--- R/errorPlot_fields.R
+++ R/errorPlot_fields.R
@@ -2,11 +2,10 @@
 #'
 #' Create a bar plot of proportion of error per field. The plot defaults to all 49 states, but location can be specified.
 #'
-#' @importFrom dplyr %>%
 #' @importFrom dplyr mutate
 #' @importFrom stringr str_extract
 #' @importFrom dplyr select
-#' @importFrom tidyr separate
+#' @importFrom tidyr separate_wider_delim
 #' @importFrom tidyr pivot_longer
 #' @importFrom dplyr filter
 #' @importFrom dplyr group_by
@@ -43,275 +42,149 @@
 
 errorPlot_fields <-
   function(x, loc = "all", year, youth = FALSE){
+    if(!loc %in% c(unique(x$dl_state), "all")) {
 
-    # Suppress warning: "Expected 25 pieces. Missing pieces filled with `NA`
-    # in ... rows". We start by splitting errors for plotting purposes; if
-    # there are less than the full amount of errors in a row, the warning
-    # happens.
-    suppressWarnings(
-      if(youth == TRUE){
-        if(loc == "all"){
+      message(
+        paste0(
+          "Error! The value you supplied to the `loc` parameter is incorrect.",
+          " Please supply a two-letter state abbreviation for the `loc`",
+          " parameter, or `loc = 'all'` to plot all available states. States",
+          " that submitted data for this download include:\n",
+          paste(unique(x$dl_state), collapse = ", "))
+      )
+    } else {
+      if (loc != "all") {
+        x <-
+          x |>
+          filter(dl_state == loc)
+      }
+      if (youth == TRUE) {
 
-          # Plot all states *with* special legend colors
+        # Plot *with* special legend colors
 
-          # Step 1: table of errors
-          table_1 <-
-            x %>%
-            mutate(
-              birth_year = str_extract(birth_date, "(?<=\\/)[0-9]{4}$"),
-              special =
-                ifelse(
-                  birth_year > year - 16,
-                  "Youth Hunter",
-                  NA)) %>%
-            select(errors, special) %>%
-            # Pull errors apart, delimited by hyphens
-            separate(errors, into = as.character(c(1:25)), sep = "-") %>%
-            # Transform errors into a single column
-            pivot_longer(1:25, names_to = "name") %>%
-            select(errors = value, special) %>%
-            filter(!is.na(errors))
+        # Step 1: table of errors
+        table_1 <-
+          x |>
+          mutate(
+            special =
+              ifelse(
+                str_extract(birth_date, "(?<=\\/)[0-9]{4}$") > year - 16,
+                "Youth Hunter",
+                NA)) |>
+          select(errors, special) |>
+          # Pull errors apart, delimited by hyphens
+          separate_wider_delim(
+            errors, delim = "-", names_sep = "_", too_few = "align_start") |>
+          # Transform errors into a single column
+          pivot_longer(starts_with("errors"), names_to = "name") |>
+          filter(!is.na(value)) |>
+          select(errors = value, special)
 
-          # Step 2: table of errors with proportions calculated -- the youth
-          # errors must be row bound in
-          table_2 <-
-            table_1 %>%
-            group_by(errors) %>%
-            # Count number of correct and incorrect values
-            summarize(count_errors = sum(!is.na(errors))) %>%
-            ungroup() %>%
-            filter(errors != "birth_date") %>%
-            bind_rows(
-              table_1 %>%
-                group_by(errors, special) %>%
-                summarize(count_errors = sum(!is.na(errors))) %>%
-                ungroup() %>%
-                filter(errors == "birth_date")) %>%
-            # Calculate error proportion
-            mutate(
-              total = nrow(x),
-              proportion = count_errors / nrow(x))
+        # Step 2: table of errors with proportions calculated -- the youth
+        # errors must be row bound in
+        table_2 <-
+          table_1 |>
+          group_by(errors) |>
+          # Count number of correct and incorrect values
+          summarize(count_errors = sum(!is.na(errors))) |>
+          ungroup() |>
+          filter(errors != "birth_date") |>
+          bind_rows(
+            table_1 |>
+              group_by(errors, special) |>
+              summarize(
+                count_errors = sum(!is.na(errors)), .groups = "drop") |>
+              filter(errors == "birth_date")) |>
+          # Calculate error proportion
+          mutate(
+            total = nrow(x),
+            proportion = count_errors / nrow(x))
 
-          # Labels for bar plot (birth_date color stack doesn't cooperate with
-          # positioning 2 labels, so we only label that field once at the top of
-          # the bar)
-          barlabels <-
-            table_2 %>%
-            select(-c("special", "total")) %>%
-            group_by(errors) %>%
-            mutate(
-              count_errors = sum(count_errors),
-              proportion = sum(proportion)) %>%
-            ungroup()
+        # Labels for bar plot (birth_date color stack doesn't cooperate with
+        # positioning 2 labels, so we only label that field once at the top of
+        # the bar)
+        barlabels <-
+          table_2 |>
+          select(-c("special", "total")) |>
+          group_by(errors) |>
+          mutate(
+            count_errors = sum(count_errors),
+            proportion = sum(proportion)) |>
+          ungroup()
 
-          # Plot
-          fields_plot <-
-            table_2 %>%
-            ggplot() +
-            geom_bar(
-              aes(x = reorder(errors, proportion), y = proportion, fill = special),
-              stat = "identity") +
-            geom_text(
-              aes(x = barlabels$errors,
-                  y = barlabels$proportion,
-                  label = barlabels$count_errors,
-                  angle = 90),
-              vjust = 0.2, hjust = -0.2) +
-            labs(
-              x = "Field",
-              y = "Error proportion",
-              title = "Error proportion per field",
-              fill = "Specifics") +
-            scale_y_continuous(expand = expansion(mult = c(-0, 0.25))) +
-            theme_classic() +
-            theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
-            scale_fill_discrete(labels = "Youth Hunter", breaks = "Youth Hunter")
-
-          return(fields_plot)
-          }
-        else{
-
-          # Plot specific state *with* special legend colors
-
-          # Step 1: table of errors
-          table_1 <-
-            x %>%
-            filter(dl_state == loc) %>%
-            mutate(
-              birth_year = str_extract(birth_date, "(?<=\\/)[0-9]{4}$"),
-              special =
-                ifelse(
-                  birth_year > year - 16,
-                  "Youth Hunter",
-                  NA)) %>%
-            select(errors, special) %>%
-            # Pull errors apart, delimited by hyphens
-            separate(errors, into = as.character(c(1:25)), sep = "-") %>%
-            # Transform errors into a single column
-            pivot_longer(1:25, names_to = "name") %>%
-            select(errors = value, special) %>%
-            filter(!is.na(errors))
-
-          # Step 2: table of errors with proportions calculated -- the youth
-          # errors must be row bound in
-          table_2 <-
-            table_1 %>%
-            group_by(errors) %>%
-            # Count number of correct and incorrect values
-            summarize(count_errors = sum(!is.na(errors))) %>%
-            ungroup() %>%
-            filter(errors != "birth_date") %>%
-            bind_rows(
-              table_1 %>%
-                group_by(errors, special) %>%
-                summarize(count_errors = sum(!is.na(errors))) %>%
-                ungroup() %>%
-                filter(errors == "birth_date")) %>%
-            # Calculate error proportion
-            mutate(
-              total = nrow(x),
-              proportion = count_errors / nrow(x))
-
-          # Labels for bar plot (birth_date color stack doesn't cooperate with
-          # positioning 2 labels, so we only label that field once at the top of
-          # the bar)
-          barlabels <-
-            table_2 %>%
-            select(-c("special", "total")) %>%
-            group_by(errors) %>%
-            mutate(
-              count_errors = sum(count_errors),
-              proportion = sum(proportion)) %>%
-            ungroup()
-
-          # Plot
-          fields_plot <-
-            table_2 %>%
-            ggplot() +
-            geom_bar(
-              aes(x = reorder(errors, proportion), y = proportion, fill = special),
-              stat = "identity") +
-            geom_text(
-              aes(
-                x = barlabels$errors,
+        # Plot
+        fields_plot <-
+          table_2 |>
+          ggplot() +
+          geom_bar(
+            aes(
+              x = reorder(errors, proportion),
+              y = proportion,
+              fill = special),
+            stat = "identity") +
+          geom_text(
+            aes(x = barlabels$errors,
                 y = barlabels$proportion,
                 label = barlabels$count_errors,
                 angle = 90),
-              vjust = 0.2, hjust = -0.2) +
-            labs(
-              x = "Field",
-              y = "Error proportion",
-              title = paste0("Error proportion per field in ", loc),
-              fill = "Specifics") +
-            scale_y_continuous(expand = expansion(mult = c(-0, 0.25))) +
-            theme_classic() +
-            theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
-            scale_fill_discrete(labels = "Youth Hunter", breaks = "Youth Hunter")
+            vjust = 0.2, hjust = -0.2) +
+          labs(
+            x = "Field",
+            y = "Error proportion",
+            fill = "Specifics") +
+          scale_y_continuous(expand = expansion(mult = c(-0, 0.25))) +
+          theme_classic() +
+          theme(
+            axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+          scale_fill_discrete(
+            labels = "Youth Hunter", breaks = "Youth Hunter")
 
-          return(fields_plot)
-          }
-        }
-        else{
-          if(loc == "all"){
+      } else {
 
-            # Plot all states without special legend colors
+        # Plot all states without special legend colors
 
-            fields_plot <-
-              x %>%
-              # # Identify the youth hunters only to have them filtered out!
-              # mutate(
-              #   birth_year = str_extract(birth_date, "(?<=\\/)[0-9]{4}$"),
-              #   special =
-              #     ifelse(
-              #       birth_year > year - 16,
-              #       "Youth Hunter",
-              #       NA)) %>%
-              # # Filter out youth hunters
-              # filter(is.na(special)) %>%
-              select(errors) %>%
-              # Pull errors apart, delimited by hyphens
-              separate(errors, into = as.character(c(1:25)), sep = "-") %>%
-              # Transform errors into a single column
-              pivot_longer(1:25, names_to = "name") %>%
-              select(errors = value) %>%
-              filter(!is.na(errors)) %>%
-              group_by(errors) %>%
-              # Count number of correct values
-              summarize(count_errors = sum(!is.na(errors))) %>%
-              ungroup() %>%
-              # Calculate error proportion
-              mutate(
-                total = nrow(x),
-                proportion = count_errors / nrow(x)) %>%
-              # Plot
-              ggplot() +
-              geom_bar(
-                aes(x = reorder(errors, proportion), y = proportion),
-                stat = "identity") +
-              geom_text(
-                aes(x = errors, y = proportion, label = count_errors, angle = 90),
-                vjust = 0.2, hjust = -0.2) +
-              labs(
-                x = "Field",
-                y = "Error proportion",
-                title = "Error proportion per field") +
-              scale_y_continuous(expand = expansion(mult = c(-0, 0.25))) +
-              theme_classic() +
-              theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+        fields_plot <-
+          x |>
+          select(errors) |>
+          # Pull errors apart, delimited by hyphens
+          separate_wider_delim(
+            errors, delim = "-", names_sep = "_", too_few = "align_start") |>
+          # Transform errors into a single column
+          pivot_longer(starts_with("errors"), names_to = "name") |>
+          select(errors = value) |>
+          filter(!is.na(errors)) |>
+          group_by(errors) |>
+          # Count number of correct values
+          summarize(count_errors = sum(!is.na(errors))) |>
+          ungroup() |>
+          # Calculate error proportion
+          mutate(
+            total = nrow(x),
+            proportion = count_errors / nrow(x)) |>
+          # Plot
+          ggplot() +
+          geom_bar(
+            aes(x = reorder(errors, proportion), y = proportion),
+            stat = "identity") +
+          geom_text(
+            aes(x = errors, y = proportion, label = count_errors, angle = 90),
+            vjust = 0.2, hjust = -0.2) +
+          labs(
+            x = "Field",
+            y = "Error proportion") +
+          scale_y_continuous(expand = expansion(mult = c(-0, 0.25))) +
+          theme_classic() +
+          theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
 
-            return(fields_plot)
-          }
-          else{
-
-            # Plot specific state without special legend colors
-
-            fields_plot <-
-              x %>%
-              # Filter data for only the desired location
-              filter(dl_state == loc) %>%
-              # Identify the youth hunters only to have them filtered out!
-              mutate(
-                birth_year = str_extract(birth_date, "(?<=\\/)[0-9]{4}$"),
-                special =
-                  ifelse(
-                    birth_year > year - 16,
-                    "Youth Hunter",
-                    NA)) %>%
-              # Filter out youth hunters
-              filter(is.na(special)) %>%
-              select(errors) %>%
-              # Pull errors apart, delimited by hyphens
-              separate(errors, into = as.character(c(1:25)), sep = "-") %>%
-              # Transform errors into a single column
-              pivot_longer(1:25, names_to = "name") %>%
-              select(errors = value) %>%
-              filter(!is.na(errors)) %>%
-              group_by(errors) %>%
-              # Count number of correct
-              summarize(count_errors = sum(!is.na(errors))) %>%
-              ungroup() %>%
-              # Calculate error proportion
-              mutate(
-                total = nrow(x),
-                proportion = count_errors / nrow(x)) %>%
-              # Plot
-              ggplot() +
-              geom_bar(
-                aes(x = reorder(errors, proportion), y = proportion),
-                stat = "identity") +
-              geom_text(
-                aes(x = errors, y = proportion, label = count_errors, angle = 90),
-                vjust = 0.2, hjust = -0.2) +
-              labs(
-                x = "Field",
-                y = "Error proportion",
-                title = paste0("Error proportion per field in ", loc)) +
-              scale_y_continuous(expand = expansion(mult = c(-0, 0.25))) +
-              theme_classic() +
-              theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
-
-            return(fields_plot)
-          }
-        }
-      )
+      }
+      if (loc == "all") {
+        return(
+          fields_plot +
+            labs(title = "Error proportion per field"))
+      } else {
+        return(
+          fields_plot +
+            labs(title = paste0("Error proportion per field in ", loc)))
+      }
+    }
   }
