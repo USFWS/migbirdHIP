c6d1a3d4d88ae1f00f0c7692bfb1767481589f24
lawalter <abby_walter@fws.gov>
2023-07-13 14:53:25 -0400

    renameFiles function: replaced %>% with |> and made other minor code improvements

--- R/renameFiles.R
+++ R/renameFiles.R
@@ -2,7 +2,6 @@
 #'
 #' This function overwrites HIP filenames. Files in the supplied directory are renamed by converting the Julian date to YYYYMMDD format. State abbreviations that are in lowercase format are capitalized.
 #'
-#' @importFrom dplyr %>%
 #' @importFrom dplyr mutate
 #' @importFrom dplyr pull
 #' @importFrom dplyr bind_cols
@@ -27,24 +26,22 @@ renameFiles <-
   function(path, year){
 
     # Add a final "/" if not included already
-    if(!str_detect(path, "\\/$")){
-      x <- paste0(path, "/")
-      }else{
-        x <- path
+    if(!str_detect(path, "\\/$")) {
+      path <- paste0(path, "/")
       }
 
     # Run if there are files in the dir with 5 digits (old Julian data format)
-    if(TRUE %in% str_detect(list.files(x), "^[A-Z]{2}[0-9]{3}(?=\\.)")){
+    if(TRUE %in% str_detect(list.files(path), "^[A-Z]{2}[0-9]{3}\\.(txt|TXT)$")){
 
       # File name(s) with 5-digit format
-      names5 <- list.files(x, pattern = "^[A-Z]{2}[0-9]{3}\\.")
+      names5 <- list.files(path, pattern = "^[A-Z]{2}[0-9]{3}\\.(txt|TXT)$")
 
       # New 10-digit file name(s)
       names10 <-
-        names5 %>%
-        as_tibble() %>%
+        names5 |>
+        as_tibble() |>
         # Pull the file name apart so we can convert the date
-        separate(value, into = c("state", "jdate", "suffix"), sep = c(2, 5)) %>%
+        separate(value, into = c("state", "jdate", "suffix"), sep = c(2, 5)) |>
         # Convert Julian date to YYYYMMDD
         # The as.Date function calculates to the jdate + 1, so subtract a day
         # using - 1 to get the accurate date; str_remove wraps around the
@@ -56,13 +53,13 @@ renameFiles <-
                 as.Date(
                   as.numeric(jdate),
                   origin = structure(paste0(as.character(year),"-01-01"))) - 1),
-              "-")) %>%
-        unite(value, 1:3, sep = "") %>%
-        mutate(value = paste0(x, value)) %>%
+              "-")) |>
+        unite(value, 1:3, sep = "") |>
+        mutate(value = paste0(path, value)) |>
         pull()
 
       # Add dir to file names
-      names5 <- paste0(x, names5)
+      names5 <- paste0(path, names5)
 
       # Overwrite the file names in the given directory
       file.rename(from = names5, to = names10)
@@ -77,26 +74,26 @@ renameFiles <-
     # Run if there's a lowercase letter in the state abbreviation
     if(TRUE %in%
        str_detect(
-         list.files(x),
+         list.files(path),
          "^([a-z]{2}|[A-Z]{1}[a-z]{1}|[a-z]{1}[A-Z]{1})(?=[0-9])")){
 
       names_lower <-
-        list.files(x) %>%
-        as_tibble() %>%
+        list.files(path) |>
+        as_tibble() |>
         filter(
           str_detect(
             value,
-            "^([a-z]{2}|[A-Z]{1}[a-z]{1}|[a-z]{1}[A-Z]{1})(?=[0-9])")) %>%
+            "^([a-z]{2}|[A-Z]{1}[a-z]{1}|[a-z]{1}[A-Z]{1})(?=[0-9])")) |>
         pull()
 
       names_upper <-
-        names_lower %>%
-        str_to_upper() %>%
+        names_lower |>
+        str_to_upper() |>
         str_replace("TXT", "txt")
 
       # Add dir to file names
-      names_lower2 <- paste0(x, names_lower)
-      names_upper2 <- paste0(x, names_upper)
+      names_lower2 <- paste0(path, names_lower)
+      names_upper2 <- paste0(path, names_upper)
 
       # Overwrite the file names in the given directory
       file.rename(from = names_lower2, to = names_upper2)
@@ -105,11 +102,11 @@ renameFiles <-
       print(
         bind_cols(old = names_lower, new = names_upper))
       }
-    if(FALSE %in% str_detect(list.files(x), "^[A-Z]{2}[0-9]{8}\\.")){
+    if(FALSE %in% str_detect(list.files(path), "^[A-Z]{2}[0-9]{8}\\.")){
       message("Error: Unresolved issue(s) with file name(s) in directory.")
       print(
-        list.files(x) %>%
-          as_tibble() %>%
+        list.files(path) |>
+          as_tibble() |>
           filter(!str_detect(value, "^[A-Z]{2}[0-9]{8}\\.")))
     }
   }
