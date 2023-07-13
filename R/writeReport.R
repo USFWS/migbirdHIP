4d5130fdff61ca5e5b43e162e15826ef30be7c9e
lawalter <abby_walter@fws.gov>
2023-07-13 15:58:33 -0400

    writeReport function: eliminated season_report template
    the season report is infrequently generated, the template is very large, and it is often annually updated anyway; for all of these reasons, it was removed from the package

--- R/writeReport.R
+++ R/writeReport.R
@@ -1,15 +1,11 @@
 #' Write an R markdown report from template
 #'
-#' Create documentation with figures and tables that summarizes HIP download data at a download cycle or season total scale.
+#' Create documentation with figures and tables that summarizes HIP download data at a download cycle scale.
 #'
 #' @importFrom quarto quarto_render
 #' @importFrom rmarkdown render
 #'
 #' @param path File path to the folder containing HIP .txt files
-#' @param type Type of report. One of the following options may be supplied:
-#' \itemize{
-#' \item dl_report - for a download cycle summary report
-#' \item season_report - for a summary of the entire HIP season}
 #' @param yr The year in which the Harvest Information Program data were collected
 #' @param dl Download cycle (when running a download report)
 #' @param corrected_path Path to directory that contains output .csv files
@@ -24,54 +20,28 @@
 #' @export
 
 writeReport <-
-  function(path, type, yr, dl = NA, corrected_path, future_path = NA, past_path = NA, dir, file){
+  function(path, yr, dl = NA, corrected_path, future_path = NA, past_path = NA, dir, file){
 
-    # Create qmd for download
-    if(type == "dl_report"){
-      #quarto_render(
-      render(
-        input =
-          # Use the specified template
-          system.file(
-            "templates",
-            paste0(type, ".Rmd"),
-            package = "migbirdHIP"),
-        # Include the specified parameters so the functions can run
-        #execute_params =
-        params =
-          list(
-            comp_path = path,
-            final_path = corrected_path,
-            future_path = future_path,
-            past_path = past_path,
-            dl = dl,
-            year = yr),
-        output_dir = dir,
-        output_file = file,
-        # Don't show lengthy knitting status text in console
-        quiet = TRUE)
+    #quarto_render(
+    render(
+      input =
+        # Use the specified template
+        system.file(
+          "templates",
+          paste0(type, ".Rmd"),
+          package = "migbirdHIP"),
+      # Include the specified parameters so the functions can run
+      #execute_params =
+      params =
+        list(
+          comp_path = path,
+          final_path = corrected_path,
+          future_path = future_path,
+          past_path = past_path,
+          dl = dl,
+          year = yr),
+      output_dir = dir,
+      output_file = file,
+      # Don't show lengthy knitting status text in console
+      quiet = TRUE)
     }
-    else if(type == "season_report"){
-      render(
-        input =
-          # Use the specified template
-          system.file(
-            "templates",
-            paste0(type, ".Rmd"),
-            package = "migbirdHIP"),
-        # Include the specified parameters so the functions can run
-        params =
-          list(
-            comp_path = path,
-            proc_path = corrected_path,
-            year = yr),
-        output_file = file,
-        output_dir = dir,
-        # Don't show lengthy knitting status text in console
-        quiet = TRUE)
-    }
-    else{
-      message("Indicated type must be 'dl_report' or 'season_report'.")
-    }
-
-  }
