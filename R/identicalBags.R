8a56fcde61c401edcfe7a1ecf4ada77b5d10a4f3
lawalter <abby_walter@fws.gov>
2023-07-31 19:36:41 -0400

    identicalBags function: created

---
+++ R/identicalBags.R
@@ -0,0 +1,138 @@
+#' Check for identical bag columns
+#'
+#' Check to make sure the data from each file don't have any columns that are exactly the same.
+#'
+#' @importFrom dplyr select
+#' @importFrom dplyr matches
+#' @importFrom tidyr expand_grid
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr row_number
+#' @importFrom tidyr pivot_longer
+#' @importFrom dplyr contains
+#' @importFrom dplyr arrange
+#' @importFrom dplyr group_by
+#' @importFrom dplyr ungroup
+#' @importFrom tidyr pivot_wider
+#' @importFrom dplyr distinct
+#' @importFrom dplyr group_split
+#' @importFrom purrr map
+#' @importFrom purrr list_rbind
+#' @importFrom dplyr pull
+#' @importFrom tibble tibble
+#' @importFrom stringr str_extract
+#' @importFrom dplyr left_join
+#' @importFrom dplyr rename
+#' @importFrom dplyr case_when
+#'
+#' @param x A HIP data tibble
+#'
+#' @author Abby Walter, \email{abby_walter@@fws.gov}
+#' @references \url{https://github.com/USFWS/migbirdHIP}
+#'
+#' @export
+
+identicalBags <-
+  function(x) {
+
+    # Get a list of species cols
+    spp <-
+      x |>
+      select(
+        matches("bag|coots|rails|cranes|pigeon|brant|seaducks")) |>
+      names()
+
+    # Get species column combinations
+    spp_combos <-
+      expand_grid(
+        spp1 = spp,
+        spp2 = spp) |>
+      filter(spp1 != spp2) |>
+      mutate(id = row_number()) |>
+      pivot_longer(cols = contains("spp")) |>
+      arrange(value) |>
+      group_by(id) |>
+      mutate(name = c("s1", "s2")) |>
+      ungroup() |>
+      pivot_wider() |>
+      distinct(s1, s2)
+
+    # Split dataframe into a list by source_file
+    split_df <-
+      x |>
+      group_by(source_file) |>
+      group_split()
+
+    # Check to see if any 2 cols from any 1 source_file are identical
+    checked_cols <-
+      map(
+        1:length(split_df),
+        function(x){
+          map(
+            1:nrow(spp_combos),
+            function(y) {
+              v1 <- pull(split_df[[x]], spp_combos$s1[[y]])
+              v2 <- pull(split_df[[x]], spp_combos$s2[[y]])
+              if(identical(v1, v2) == TRUE) {
+                tibble(
+                  source_file = unique(split_df[[x]]$source_file),
+                  spp1 = spp_combos$s1[[y]],
+                  spp2 = spp_combos$s2[[y]],
+                  n = length(v1),
+                  value = v1[1])
+              }
+            }) |>
+            list_rbind()
+        }) |>
+        list_rbind() |>
+        mutate(dl_state = str_extract(source_file, "^[A-Z]{2}")) |>
+        # Keep only the spp groups with more than 1 possible bag value
+        # i.e. filter out "no season" species/states
+        left_join(
+          hip_bags_ref |>
+            select(-FWSstratum) |>
+            group_by(state, spp) |>
+            filter(n() == 1) |>
+            ungroup() |>
+            rename(
+              dl_state = state,
+              value = stateBagValue,
+              spp1 = spp) |>
+            mutate(
+              value = as.character(value),
+              flag1 = "no season"),
+          by = c("dl_state", "spp1", "value")
+        ) |>
+        left_join(
+          hip_bags_ref |>
+            select(-FWSstratum) |>
+            group_by(state, spp) |>
+            filter(n() == 1) |>
+            ungroup() |>
+            rename(
+              dl_state = state,
+              value = stateBagValue,
+              spp2 = spp) |>
+            mutate(
+              value = as.character(value),
+              flag2 = "no season"),
+          by = c("dl_state", "spp2", "value")
+        ) |>
+        select(-dl_state) |>
+        filter(is.na(flag1)|is.na(flag2)) |>
+        # Add notes specifying which species have a season
+        mutate(
+          note =
+            case_when(
+              is.na(flag1) & is.na(flag2) ~ paste0("Both seasons exist"),
+              is.na(flag1) ~ paste0(spp1, " has a season"),
+              is.na(flag2) ~ paste0(spp2, " has a season"),
+              TRUE ~ NA_character_
+            )) |>
+        select(-c("flag1", "flag2"))
+
+    if(nrow(checked_cols) > 0) {
+      return(checked_cols)
+    } else {
+      message("Data are good to go! No identical columns detected.")}
+  }
