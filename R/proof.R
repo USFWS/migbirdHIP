222f639cb7090e986d836a6ce25f98480fd7a58f
lawalter <abby_walter@fws.gov>
2023-09-01 12:52:56 -0400

    proof function: regex for proofing updated; registration_yr error messages eliminated

--- R/proof.R
+++ R/proof.R
@@ -53,16 +53,16 @@ proof <-
               case_when(
                 # First name should be >1 letter
                 str_detect(firstname, "^[A-Z]{1}$") ~ "firstname",
-                # First name should not contain a first initial/middle name
-                str_detect(firstname, "^[A-Z]{1}(?=\\s)") ~ "firstname",
-                # First name should not contain a first name/middle initial
-                str_detect(firstname, ".+(?<=\\s)[A-Z]{1}$") ~ "firstname",
-                # Only non-alpha characters allowed are spaces and hyphens
-                # no numbers, commas, parentheses, etc
-                str_detect(firstname, "[^A-Z\\s\\-]") ~ "firstname",
+                # First name should not contain 1 letter followed by a name
+                str_detect(firstname, "^[A-Z]{1}\\s[A-Z]+$") ~ "firstname",
+                # First name should not contain a name followed by 1 letter
+                str_detect(firstname, "^[A-Z]+\\s[A-Z]{1}$") ~ "firstname",
+                # Only non-alpha characters allowed are spaces, apostrophes, and
+                # hyphens. No numbers, commas, parentheses, etc allowed
+                str_detect(firstname, "[^A-Z\\s\\-\\']") ~ "firstname",
                 # No full names (detect using 2+ spaces)
                 str_detect(
-                  firstname, "^.+[(?<=//s)[a-z](?=//s)].+$") ~ "firstname",
+                  firstname, "^[A-Z]+\\s[A-Z]+\\s[A-Z]+$") ~ "firstname",
                 # Should not be "blank" or "inaudible"
                 str_detect(
                   firstname, "^[INAUDIBLE|BLANK|TEST|USER|RESIDENT]$") ~
@@ -70,7 +70,7 @@ proof <-
                 TRUE ~ NA_character_)),
         # Middle name should only be 1 letter of the alphabet
         keyed_x |>
-          filter(str_detect(middle, "[^A-Z]{1}")) |>
+          filter(!str_detect(middle, "^[A-Z]{1}$")) |>
           mutate(error = "middle"),
         # Last name
         keyed_x |>
@@ -84,9 +84,9 @@ proof <-
                 str_detect(lastname, "[^A-Z\\s\\-\\.\\']") ~ "lastname",
                 # No full names (detect using 2+ spaces)
                 str_detect(
-                  lastname, "^.+[(?<=//s)[a-z](?=//s)].+$") ~ "lastname",
+                  lastname, "^[A-Z]+\\s[A-Z]+\\s[A-Z]+$") ~ "lastname",
                 # Should not be "inaudible"
-                str_detect(lastname, "^[INAUDIBLE]$") ~ "lastname",
+                str_detect(lastname, "^INAUDIBLE$") ~ "lastname",
                 TRUE ~ NA_character_)),
         # Suffix
         # Allows 1-20 in Roman numerals and numeric, excluding XVIII since the
@@ -100,49 +100,39 @@ proof <-
         # Address does not contain |, tab or non-UTF8 characters
         # Any further address verification isn't really possible
         keyed_x |>
-          filter(
-            str_detect(
-              address, "\\||\\t|\\x00-\\x7F")) |>
+          filter(str_detect(address, "\\||\\t|[^\\x00-\\x7F]+")) |>
           mutate(error = "address"),
-        # City should only contain letters
+        # City should only contain letters, spaces (e.g. New York City), hyphens
+        # (e.g. Winston-Salem, NC), or apostrophes (e.g. HI residents and
+        # O'Fallon, MO)
         keyed_x |>
-          filter(!str_detect(city, "[A-Z|a-z]")) |>
+          filter(str_detect(city, "[^A-Za-z\\s\\-\\']")) |>
+          mutate(error = "city"),
+        # City MUST contain at least 3 letters
+        keyed_x |>
+          filter(str_detect(city, "^[A-Za-z]{1,2}$")) |>
           mutate(error = "city"),
         # State should only contain a specific list of states/provinces/etc
         keyed_x |>
           filter(!str_detect(state, states_provinces_and_canada)) |>
           mutate(error = "state"),
-        # Zip code should contain 5 digits or 5 digits-4 digits
+        # Zip code should be in the reference table
         keyed_x |>
-          mutate(
-            error =
-              case_when(
-                str_detect(zip, "^[0-9]{9}$") ~ "zip",
-                str_detect(zip, "\\-0000$") ~ "zip",
-                str_detect(zip, "[^[0-9]{5}|[0-9]{5}\\-[0-9]{4}]") ~ "zip",
-                TRUE ~ NA_character_)),
-        # Birth date should only ever be 100 - 16 years go, since hunter do not
-        # have to register if less than 16 years of age
+          filter(!str_extract(zip, "^[0-9]{5}") %in% zip_code_ref$zipcode) |>
+          mutate(error = "zip"),
+        # Birth date should only ever be between 100 and 16 years go, since
+        # hunters do not have to register if less than 16 years of age & years
+        # from earlier than 100 years ago are unlikely
         keyed_x |>
           filter(
             as.numeric(
-              str_extract(birth_date, "(?<=\\/)[0-9]{4}")) < 1920 |
+              str_extract(birth_date, "(?<=\\/)[0-9]{4}")) < year - 100 |
               as.numeric(
                 str_extract(birth_date, "(?<=\\/)[0-9]{4}")) > year - 16) |>
           mutate(error = "birth_date"),
-        # Issue date should only ever be the year +/- 1 of the HIP survey
-        keyed_x |>
-          filter(
-            as.numeric(
-              str_extract(issue_date, "(?<=\\/)[0-9]{4}")) != year &
-              as.numeric(
-                str_extract(issue_date, "(?<=\\/)[0-9]{4}")) != year + 1 &
-              as.numeric(
-                str_extract(issue_date, "(?<=\\/)[0-9]{4}")) != year - 1) |>
-          mutate(error = "issue_date"),
         # Hunting migratory birds should only be = 1 or 2
         keyed_x |>
-          filter(!str_detect(hunt_mig_birds, "1|2")) |>
+          filter(!hunt_mig_birds %in% c("1", "2")) |>
           mutate(error = "hunt_mig_birds"),
         # Registration year should = survey year
         keyed_x |>
@@ -152,41 +142,10 @@ proof <-
         keyed_x |>
           filter(
             !str_detect(
-              email, "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")) |>
+              email, "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$")) |>
           mutate(error = "email")
       ) |>
       select(temp_key, error)
-
-    # Registration_yr + 1 error
-    # Return a message if registration_yr is yr +/- 1; these values should have
-    # been handled in issueCheck
-    if((year + 1) %in% keyed_x$registration_yr){
-      message(paste0("Error: Records detected with registration_yr = yr + 1."))
-      print(
-        keyed_x |>
-          group_by(source_file) |>
-          mutate(total_n = n()) |>
-          ungroup() |>
-          filter(registration_yr == year + 1) |>
-          group_by(source_file) |>
-          reframe(
-            n = n(),
-            prop = round(n()/total_n, 2)) |>
-          distinct())
-    }else if((year - 1) %in% keyed_x$registration_yr){
-      message(paste0("Error: Records detected with registration_yr = yr - 1."))
-      print(
-        keyed_x |>
-          group_by(source_file) |>
-          mutate(total_n = n()) |>
-          ungroup() |>
-          filter(registration_yr == year - 1) |>
-          group_by(source_file) |>
-          reframe(
-            n = n(),
-            prop = round(n()/total_n, 2)) |>
-          distinct())
-    }
 
     graded_x <-
       keyed_x |>
@@ -209,10 +168,9 @@ proof <-
       mutate(errors = ifelse(str_detect(errors, "^NA$"), NA, errors)) |>
       as_tibble()
 
-    # Proof the zip codes -- are they associated with the correct states?
+    # Proof the zip codes. Are they associated with the correct states?
     graded_x <-
       graded_x |>
-      # Proof the zip codes -- are they associated with the correct states?
       left_join(
         zip_code_ref |>
           distinct(zip = zipcode, zipState = state),
