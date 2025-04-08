fcb0b0378c06d015eead1d20db65143c6ace7d88
Walter <abby_walter@fws.gov>
2025-04-08 16:42:20 -0400

    logo: enlarged map to increase AK visibility

--- inst/logo/logo.R
+++ inst/logo/logo.R
@@ -11,33 +11,32 @@ wodu <- readPNG("man/figures/wodu_silhouette_sm.png", native = TRUE)
 
 usa49 <-
   plot_usmap(
+    regions = "state",
     data = statepop,
-    values = "pop_2015",
+    values = "pop_2022",
     fill = "#FFFFFF", color = "#0270bb", exclude = "Hawaii") +
   theme(
     plot.background = element_rect(fill = NA, color = NA),
-    plot.margin = unit(c(0, 0, 0, 0), "cm"))
+    plot.margin = unit(c(0, .2, 0, 0), "cm"))
 
-orange_wodu <-
+map_wodu <-
   ggdraw(usa49) +
-  draw_image(wodu, x = 0, y = .1, width = .95, height = .9)
+  draw_image(wodu, x = 0, y = .11, width = .95, height = .9)
 
 # PNG sticker
-MBHS_hex <- hexSticker::sticker(
-  orange_wodu,
+hexSticker::sticker(
+  map_wodu,
   package = "migbirdHIP", p_color = "#FFFFFF",
-  p_size = 19, s_x = 1, s_y = .75, s_width = 1.2, s_height = .9,
+  p_size = 19, s_x = 1, s_y = .75, s_width = 1.4, s_height = .9,
   h_fill = "#faa225", h_color = "#ffd151",
   dpi = 320,
   filename = "man/figures/logo.png")
-print(MBHS_hex)
 
 # SVG sticker
-MBHS_hex <- hexSticker::sticker(
-  orange_wodu,
+hexSticker::sticker(
+  map_wodu,
   package = "migbirdHIP", p_color = "#FFFFFF",
-  p_size = 6, s_x = 1, s_y = .75, s_width = 1.2, s_height = .9,
+  p_size = 6, s_x = 1, s_y = .75, s_width = 1.4, s_height = .9,
   h_fill = "#faa225", h_color = "#ffd151",
   dpi = 320,
   filename = "man/figures/logo.svg")
-print(MBHS_hex)
