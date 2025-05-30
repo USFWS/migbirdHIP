9819710af5888c37152ebe84ebaeb10a6c65d025
Walter <abby_walter@fws.gov>
2025-05-30 11:17:45 -0400

    test read_hip

--- tests/testthat/test-read_hip.R
+++ tests/testthat/test-read_hip.R
@@ -1,3 +1,30 @@
+# read_hip function -------------------------------------------------------
+
+test_that("read_hip works for single state", {
+
+  suppressMessages(
+    invisible(
+      capture.output(
+        read_data <-
+          read_hip(
+            paste0(here::here(), "/inst/extdata/DL0901/"),
+            state = "RI"))))
+
+  expect_true(nrow(read_data) > 0)
+})
+
+test_that("read_hip works for a download", {
+
+  suppressMessages(
+    invisible(
+      capture.output(
+        read_data <-
+          read_hip(
+            paste0(here::here(), "/inst/extdata/DL0901/")))))
+
+  expect_true(length(unique(read_data$dl_state)) == 49)
+})
+
 # test record -------------------------------------------------------------
 
 # see test-proof.R
