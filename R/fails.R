9b7b65437961f41c194afe05ed853424b60400c5
Abby Walter <abby_walter@fws.gov>
2025-06-26 10:45:57 -0400

    add failyear function to reduce repeated stopifnot conditions across other package functions

---
+++ R/fails.R
@@ -0,0 +1,16 @@
+#' Fail if incorrect year supplied
+#'
+#' Internal function that fails if an incorrect year is provided.
+#'
+#' @importFrom stringr str_detect
+#'
+#' @param year The year in which the Harvest Information Program data were collected
+#'
+#' @author Abby Walter, \email{abby_walter@@fws.gov}
+#' @references \url{https://github.com/USFWS/migbirdHIP}
+
+failyear <-
+  function(year) {
+    stopifnot("Error: `year` parameter must be numeric." = is.numeric(year))
+    stopifnot("Error: Incorrect value supplied for `year` parameter. Please use a 4-digit year in the 2020s, e.g. 2024." = str_detect(year, "^202[0-9]{1}$"))
+  }
