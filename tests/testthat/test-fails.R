56fe044f809ba65c896ee9f35e8476c39f7ab484
Abby Walter <abby_walter@fws.gov>
2025-07-17 14:49:51 -0400

    add unit test for failState

--- tests/testthat/test-fails.R
+++ tests/testthat/test-fails.R
@@ -1,17 +1,23 @@
-test_that("failYear works", {
-  expect_error(failYear("A"))
-  expect_error(failYear("2019"))
-  expect_error(failYear(2019))
-  expect_error(failYear(2024.5))
-  expect_error(failYear(2030))
-  expect_no_error(failYear(2024))
-})
-
 test_that("failProofed works", {
   expect_error(failProofed(DF_TEST_MINI))
 
   test_failproofed <- DF_TEST_MINI |> dplyr::mutate(errors = NA)
   expect_no_error(failProofed(test_failproofed))
+})
+
+test_that("failState works", {
+  expect_error(failState("A"))
+  expect_error(failState("MX"))
+  expect_error(failState("SMITH"))
+  expect_error(failState("XT"))
+  expect_error(failState("Arizona"))
+  expect_error(failState("2019"))
+  expect_error(failState(2019))
+  expect_error(failState(2024.5))
+
+  for(i in seq_along(REF_ABBR_49_STATES)) {
+    expect_no_error(failState(REF_ABBR_49_STATES[i]))
+  }
 })
 
 test_that("failTF works", {
@@ -23,4 +29,13 @@ test_that("failTF works", {
   expect_no_error(failTF(FALSE))
   expect_no_error(failTF(T))
   expect_no_error(failTF(F))
+})
+
+test_that("failYear works", {
+  expect_error(failYear("A"))
+  expect_error(failYear("2019"))
+  expect_error(failYear(2019))
+  expect_error(failYear(2024.5))
+  expect_error(failYear(2030))
+  expect_no_error(failYear(REF_CURRENT_SEASON))
 })
